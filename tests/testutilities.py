1115b163adf8a385501c22ed205b03dc368a66e1
Timothy Claeys <timothy.claeys@imag.fr>
2018-08-19 12:02:21 +0200

    Fix in testutilities

--- tests/testutilities.py
+++ tests/testutilities.py
@@ -1,6 +1,4 @@
-import difflib
 import json
-import re
 from os import listdir, path
 
 
@@ -17,12 +15,3 @@ class TestUtilities:
         with open(file, 'r') as f:
             current_test_case = json.loads(f.read())
         return current_test_case
-
-
-if __name__ == "__main__":
-    print(next(TestUtilities.get_next_test("../testcases/mac0-tests")))
-
-    str1 = "zuefzepfibuz"
-    str2 = "zuefaazepfibuz"
-
-    print(TestUtilities.find_failures(str1, str2))
