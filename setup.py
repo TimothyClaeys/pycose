af7e144c966fc8dabe108a747f6dbc0c4ff2f834
Timothy Claeys <timothy.claeys@gmail.com>
2020-07-24 14:52:41 +0200

    Fix setup procedure

--- setup.py
+++ setup.py
@@ -1,16 +1,42 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+def _read_requirements(file_name):
+    """
+    Returns list of required modules for 'install_requires' parameter. Assumes
+    requirements file contains only module lines and comments.
+    """
+    requirements = []
+    with open(os.path.join(file_name)) as f:
+        for line in f:
+            if not line.startswith('#'):
+                requirements.append(line)
+    return requirements
+
+
+INSTALL_REQUIREMENTS = _read_requirements('requirements.txt')
+
+# README as long description
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name='pycose',
-    version='0.1.1',
+    version='0.1.3',
     packages=find_packages(exclude=['tests']),
-    python_requires='>=3.3',
-    install_requires=[
-        'cryptography',
-        'cbor',
-        'ecdsa',
-    ],
-
+    python_requires='>=3.5',
+    package_data={
+        '': [
+            'requirements.txt',
+            'images/*.png',
+        ],
+    },
+    install_requires=INSTALL_REQUIREMENTS,
+    long_description=LONG_DESCRIPTION,
+    keywords=['COSE', 'Internet of Things', 'CBOR', 'object security', 'EDHOC', 'OSCORE'],
     author='Timothy Claeys',
     author_email='timothy.claeys@gmail.com',
     license='BSD-3',
