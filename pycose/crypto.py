43c0babd5103ca7fd26740b18cd8b101ddf4d094
Timothy Claeys <timothy.claeys@gmail.com>
2020-08-05 00:01:56 +0200

    Add key derivation function

--- pycose/crypto.py
+++ pycose/crypto.py
@@ -1,68 +1,53 @@
 import hashlib
 from os import urandom
 
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.backends import default_backend, openssl
 from cryptography.hazmat.primitives import cmac
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import hmac
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey, EllipticCurvePublicKey, ECDH
 from cryptography.hazmat.primitives.ciphers import algorithms, aead
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.keywrap import aes_key_wrap
 from ecdsa import curves
-from ecdsa import keys
 
 from pycose.attributes import CoseAlgorithm
 from pycose.exceptions import *
 
 aes_key_wraps = {CoseAlgorithm.A128KW, CoseAlgorithm.A192KW, CoseAlgorithm.A256KW}
 
-hashes_for_ecc = \
-    {
-        'ES256': hashlib.sha256,
-        'ES384': hashlib.sha384,
-        'ES512': hashlib.sha512,
-    }
+HMAC = {
+    CoseAlgorithm.HMAC_256_64: hashes.SHA256,
+    CoseAlgorithm.HMAC_256_256: hashes.SHA256,
+    CoseAlgorithm.HMAC_384_384: hashes.SHA384,
+    CoseAlgorithm.HMAC_512_512: hashes.SHA512,
+}
 
-hmacs = \
-    {
-        'HMAC 256/64': hashes.SHA256,
-        'HMAC 256/256': hashes.SHA256,
-        'HMAC 384/384': hashes.SHA384,
-        'HMAC 512/512': hashes.SHA512,
-    }
+CMAC = {
+    CoseAlgorithm.AES_MAC_256_64: algorithms.AES,
+    CoseAlgorithm.AES_MAC_128_64: algorithms.AES,
+    CoseAlgorithm.AES_MAC_256_128: algorithms.AES,
+    CoseAlgorithm.AES_MAC_128_128: algorithms.AES,
+}
 
-cmacs = \
-    {
-        'AES-MAC 256/64': algorithms.AES,
-        'AES-MAC-256/128': algorithms.AES,
-        'AES-MAC 128/64': algorithms.AES,
-        'AES-MAC 128/128': algorithms.AES,
-    }
-
-aeads = \
-    {
-        CoseAlgorithm.A128GCM: (aead.AESGCM, 16),
-        CoseAlgorithm.A192GCM: (aead.AESGCM, 16),
-        CoseAlgorithm.A256GCM: (aead.AESGCM, 16),
-        CoseAlgorithm.AES_CCM_16_64_128: (aead.AESCCM, 8),
-        CoseAlgorithm.AES_CCM_16_64_256: (aead.AESCCM, 8),
-        CoseAlgorithm.AES_CCM_64_64_128: (aead.AESCCM, 8),
-        CoseAlgorithm.AES_CCM_64_64_256: (aead.AESCCM, 8),
-        CoseAlgorithm.AES_CCM_16_128_128: (aead.AESCCM, 16),
-        CoseAlgorithm.AES_CCM_16_128_256: (aead.AESCCM, 16),
-        CoseAlgorithm.AES_CCM_64_128_256: (aead.AESCCM, 16),
-        CoseAlgorithm.AES_CCM_64_128_128: (aead.AESCCM, 16),
-    }
-
-ec_curves = \
-    {
-        'P-256': curves.NIST256p,
-        'P-384': curves.NIST384p,
-    }
+AEAD = {
+    CoseAlgorithm.A128GCM: (aead.AESGCM, 16),
+    CoseAlgorithm.A192GCM: (aead.AESGCM, 16),
+    CoseAlgorithm.A256GCM: (aead.AESGCM, 16),
+    CoseAlgorithm.AES_CCM_16_64_128: (aead.AESCCM, 8),
+    CoseAlgorithm.AES_CCM_16_64_256: (aead.AESCCM, 8),
+    CoseAlgorithm.AES_CCM_64_64_128: (aead.AESCCM, 8),
+    CoseAlgorithm.AES_CCM_64_64_256: (aead.AESCCM, 8),
+    CoseAlgorithm.AES_CCM_16_128_128: (aead.AESCCM, 16),
+    CoseAlgorithm.AES_CCM_16_128_256: (aead.AESCCM, 16),
+    CoseAlgorithm.AES_CCM_64_128_256: (aead.AESCCM, 16),
+    CoseAlgorithm.AES_CCM_64_128_128: (aead.AESCCM, 16),
+}
 
 
 def aead_encrypt(key, aad, plaintext, algorithm, nonce):
     try:
-        primitive, tag_length = aeads[algorithm]
+        primitive, tag_length = AEAD[algorithm]
         if tag_length != 16:
             aead_cipher = primitive(key, tag_length=tag_length)
         else:
@@ -76,7 +61,7 @@ def aead_encrypt(key, aad, plaintext, algorithm, nonce):
 
 def aead_decrypt(key, aad, ciphertext, algorithm, nonce):
     try:
-        primitive = aeads[algorithm]
+        primitive = AEAD[algorithm]
         aesgcm = primitive(key)
         plaintext = aesgcm.decrypt(nonce, ciphertext, aad)
     except KeyError as err:
@@ -102,7 +87,7 @@ def calc_tag_wrapper(key, to_be_maced, algorithm):
     """
 
     try:
-        primitive = cmacs[algorithm]
+        primitive = CMAC[algorithm]
         c = cmac.CMAC(primitive(key), backend=default_backend())
         c.update(to_be_maced)
         digest = c.finalize()
@@ -112,7 +97,7 @@ def calc_tag_wrapper(key, to_be_maced, algorithm):
             digest = digest[:8]
     except KeyError:
         try:
-            primitive = hmacs[algorithm]
+            primitive = HMAC[algorithm]
             h = hmac.HMAC(key, primitive(), backend=default_backend())
             h.update(to_be_maced)
             digest = h.finalize()
@@ -130,7 +115,7 @@ def calc_tag_wrapper(key, to_be_maced, algorithm):
 def verify_tag_wrapper(key, tag, to_be_maced, algorithm):
     if algorithm != 'HS256/64':
         try:
-            hash_primitive = hmacs[algorithm]
+            hash_primitive = HMAC[algorithm]
         except KeyError as e:
             raise CoseUnsupportedMAC("This cipher is not supported by the COSE specification: {}".format(e))
 
@@ -139,7 +124,7 @@ def verify_tag_wrapper(key, tag, to_be_maced, algorithm):
         h.verify(tag)
     elif algorithm == 'HS256/64':
         try:
-            hash_primitive = hmacs[algorithm]
+            hash_primitive = HMAC[algorithm]
         except KeyError as e:
             raise CoseUnsupportedMAC("This cipher is not supported by the COSE specification: {}".format(e))
 
@@ -150,35 +135,35 @@ def verify_tag_wrapper(key, tag, to_be_maced, algorithm):
     return True
 
 
-def ec_sign_wrapper(key, to_be_signed, algorithm='ES256', curve='P-256'):
-    if isinstance(key, str):
-        signer = derive_priv_key(key, ec_curves[curve], hashfunc=hashes_for_ecc[algorithm])
-    else:
-        signer = key
-    return signer.sign_deterministic(to_be_signed, hashfunc=hashes_for_ecc[algorithm])
+def ecdh_key_derivation(private_key: EllipticCurvePrivateKey,
+                        public_key: EllipticCurvePublicKey,
+                        length: int,
+                        context: bytes = b''):
+    shared_key = private_key.exchange(ECDH(), public_key)
 
+    derived_key = HKDF(algorithm=hashes.SHA256(),
+                       length=length,
+                       salt=None,
+                       info=context,
+                       backend=openssl.backend).derive(shared_key)
 
-def ec_verify_wrapper(key, to_be_signed, signature, algorithm='ES256', curve='P-256'):
-    if isinstance(key, str):
-        signer = derive_priv_key(key, ec_curves[curve], hashfunc=hashes_for_ecc[algorithm])
-    else:
-        signer = key
-    try:
-        verifier = signer.get_verifying_key()
-    except AttributeError:
-        verifier = signer
-    return verifier.verify(signature, to_be_signed, hashfunc=hashes_for_ecc[algorithm])
+    return shared_key, derived_key
 
-
-def generate_crypto_keys(algorithm='ES256', curve='P-256'):
-    if algorithm in ['ES256', 'ES384', 'ES512']:
-        return keys.SigningKey.generate(ec_curves[curve], hashfunc=hashes_for_ecc[algorithm])
-    if algorithm in ['HS256/64', 'HS256', 'HS384', 'HS512']:
-        seed = get_randomness(64)
-        key = hashes.Hash(hmacs[algorithm](), backend=default_backend())
-        key.update(seed)
-        return key.finalize()
-
-
-def get_randomness(bytecount):
-    return urandom(bytecount)
+# def ec_sign_wrapper(key, to_be_signed, algorithm, curve):
+#     if isinstance(key, str):
+#         signer = derive_priv_key(key, ec_curves[curve], hashfunc=hashes_for_ecc[algorithm])
+#     else:
+#         signer = key
+#     return signer.sign_deterministic(to_be_signed, hashfunc=hashes_for_ecc[algorithm])
+#
+#
+# def ec_verify_wrapper(key, to_be_signed, signature, algorithm='ES256', curve='P-256'):
+#     if isinstance(key, str):
+#         signer = derive_priv_key(key, ec_curves[curve], hashfunc=hashes_for_ecc[algorithm])
+#     else:
+#         signer = key
+#     try:
+#         verifier = signer.get_verifying_key()
+#     except AttributeError:
+#         verifier = signer
+#     return verifier.verify(signature, to_be_signed, hashfunc=hashes_for_ecc[algorithm])
