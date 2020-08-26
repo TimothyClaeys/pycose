995fc8f56c110a65d79691df6449b69489bd36d2
Timothy Claeys <timothy.claeys@gmail.com>
2020-08-26 15:00:58 +0200

    Add x5u and x5chain types

--- pycose/x509.py
+++ pycose/x509.py
@@ -44,3 +44,29 @@ class X5T:
 
     def encode(self):
         return [self.alg, self.compute_hash()]
+
+
+class X5U:
+    def __init__(self, uri: str):
+        self.uri = uri
+
+    def encode(self):
+        return self.uri
+
+
+class X5Chain:
+    def __init__(self, cert_chain: Union[List[bytes], bytes], verify: bool = False):
+        if isinstance(cert_chain, list) and len(cert_chain) == 1:
+            self.cert_chain = cert_chain[0]
+        else:
+            self.cert_chain = cert_chain
+
+        if verify:
+            self.verify_chain()
+
+    def verify_chain(self):
+        # TODO: verify certificate chain
+        pass
+
+    def encode(self) -> Union[bytes, List[bytes]]:
+        return self.cert_chain
