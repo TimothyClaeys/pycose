f8a58fccda123c94e150fe185c74475399bc55a3
Timothy Claeys <timothy.claeys@gmail.com>
2021-04-15 14:50:33 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(os.path.join(this_directory, 'README.md')) as file:
 
 setup(
     name='cose',
-    version='0.9.dev4',
+    version='0.9.dev5',
     packages=find_packages(exclude=['tests', 'docs']),
     python_requires='>=3.6',
     include_package_data=True,
